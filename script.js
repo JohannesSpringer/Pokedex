17ea0f417fe10baffe6ef6d6cd97f92c63d47a4f
JohannesSpringer <johannes.springer.01@gmx.de>
2023-02-10 16:26:45 +0100

    added info table

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let currentPokemon;
 
 async function loadPokemon() {
-    let url = `https://pokeapi.co/api/v2/pokemon/charmander`;
-    //let url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
+    //let url = `https://pokeapi.co/api/v2/pokemon/charmander`;
+    let url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
     let response = await fetch(url);
     currentPokemon = await response.json();
     console.log(currentPokemon);
@@ -15,6 +15,7 @@ function renderPokemonInfo() {
     document.getElementById('pokemonImg').src = currentPokemon['sprites']['other']['home']['front_default'];
     document.getElementById('pokemonId').innerHTML = '#' + String(currentPokemon['id']).padStart(3, '0');
     renderPokemonTypes();
+    renderPokemonStats();
 }
 
 function renderPokemonTypes() {
@@ -25,5 +26,24 @@ function renderPokemonTypes() {
         document.getElementById('pokemonTypes').innerHTML += `
             <span class="pokemon-type">${type}</span>
         `;
+    }
+}
+
+function renderPokemonStats() {
+    let stats = currentPokemon['stats'];
+    document.getElementById('pokemonInfoTable').innerHTML = '';
+    for (let i = 0; i < stats.length; i++) {
+        const stat = stats[i];
+        document.getElementById('pokemonInfoTable').innerHTML += `
+        <tr>
+            <td>${stat['stat']['name']}</td>
+            <td>${stat['base_stat']}</td>
+            <td>
+            <div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="25"
+                aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar bg-success" style="width: 25%">100</div>
+            </div>
+            </td>
+        </tr>`;
     }
 }
